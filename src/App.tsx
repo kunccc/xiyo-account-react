88e19bffd767306ee22069ed7148dec0702c9081
Hee <kuncc@foxmail.com>
2021-05-03 20:02:49 +0800

    update: 使用 react-router 完成导航栏基本功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,59 @@
+import {HashRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 import React from 'react';
+import styled from 'styled-components';
+import Nav from './components/Nav';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
 
 function App() {
   return (
-    <div>hi</div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistic">
+              <Statistic/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav/>
+      </Wrapper>
+    </Router>
   );
+}
+
+function Tags() {
+  return <h2>标签</h2>;
+}
+
+function Money() {
+  return <h2>记账</h2>;
+}
+
+function Statistic() {
+  return <h2>数据</h2>;
+}
+
+function NoMatch() {
+  return <h2>路径错误</h2>;
 }
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,38 @@
+import {Link} from 'react-router-dom';
+import React from 'react';
+import styled from 'styled-components';
+
+const NavWrapper = styled.nav`
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
+  ul {
+    display: flex;
+    align-items: center;
+    height: 48px;
+
+    > li {
+      width: 33.33%;
+      text-align: center;
+    }
+  }
+`;
+
+function Nav() {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/tags">标签</Link>
+        </li>
+        <li>
+          <Link to="/money">记账</Link>
+        </li>
+        <li>
+          <Link to="/statistic">数据</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+}
+
+export default Nav;
