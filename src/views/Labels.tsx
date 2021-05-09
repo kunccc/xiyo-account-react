963529e2f4ade8970d3b1e74157fe25ef3c416cb
KunC <kuncc@foxmail.com>
2021-05-09 10:09:04 +0800

    update: 完成 Labels 页面布局

--- src/views/Labels.tsx
+++ src/views/Labels.tsx
@@ -1,11 +1,43 @@
 import Layout from 'components/Layout';
 import React from 'react';
+import {useTags} from '../lib/useTags';
+import Icon from 'components/Icon';
+import styled from 'styled-components';
+
+const Ol = styled.ol`
+  li {
+    height: 46px;
+    padding: 0 8px 0 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #ccc;
+    .tag {
+      display: flex;
+      align-items: flex-end;
+      .icon {
+        margin-right: 8px;
+      }
+    }
+  }
+`;
 
 const Labels: React.FC = () => {
-  const selectTab = (selectedTab: string) => {console.log(selectedTab)}
+  const {tagsSourceForPay, tagsSourceForIncome} = useTags();
+  const [tags, setTags] = React.useState(tagsSourceForPay);
+  const selectTab = (selectedTab: string) => {
+    setTags(selectedTab === 'pay' ? tagsSourceForPay : tagsSourceForIncome);
+  };
   return (
     <Layout selectTab={selectTab}>
-      <h2>标签</h2>
+      <Ol>
+        {tags.map(tag =>
+          <li key={tag.id}>
+            <div className="tag"><Icon name={tag.enName}/>{tag.chName}</div>
+            <Icon name="rubbish"/></li>
+        )}
+        <li>添加新标签<Icon name="add"/></li>
+      </Ol>
     </Layout>
   );
 };
